92e55dc633dabeeb71de7c6d028a8adddfa56e61
Maxime Sbaizero <maximesbaizero@gmail.com>
2024-10-28 11:05:12 +0100

    ajout du token

--- models/users.js
+++ models/users.js
@@ -15,6 +15,7 @@ const userSchema = new mongoose.Schema({
     lastname: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
+    token: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
     inscription_date: { type: Date, default: Date.now },
     favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'articles' }],

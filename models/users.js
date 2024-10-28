add76f802ea7b06dee4198ecc1b6f012e74bc7b2
antoinekc <kimkimkimkim0719@gmail.com>
2024-10-28 14:27:09 +0100

    routes Users fonctionnelles

---
+++ models/users.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const adresseSchema = new mongoose.Schema({
+    line1: { type: String },
+    line2: { type: String },
+    line3: { type: String },
+    zip_code: { type: Number },
+    city: { type: String },
+    country: { type: String },
+    infos: { type: String },
+});
+
+const userSchema = new mongoose.Schema({
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    token: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+    inscription_date: { type: Date, default: Date.now },
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'articles' }],
+    adresses: [adresseSchema],
+});
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User;

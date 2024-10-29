72326d3970bcc73fe19ae507d054f628ebc55f4c
Maxime Sbaizero <maximesbaizero@gmail.com>
2024-10-29 12:41:56 +0100

    ajout des routes pour le profile

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const uid2 = require("uid2");
 const bcrypt = require("bcrypt");
 
 router.post("/signup", (req, res) => {
-  if (!checkBody(req.body, ["firstname","lastname", "password"])) {
+  if (!checkBody(req.body, ["firstname", "lastname", "password"])) {
     res.json({ result: false, error: "Missing or empty fields" });
     return;
   }
@@ -20,11 +20,11 @@ router.post("/signup", (req, res) => {
 
       const email = req.body.email
 
-      if(emailPattern.test(email)) {
-        res.json({ message: "Email valide"})
+      if (emailPattern.test(email)) {
+        res.json({ message: "Email valide" })
       } else {
-        res.json({ message: "Email non valide"})
-      } 
+        res.json({ message: "Email non valide" })
+      }
 
       const newUser = new User({
         firstname: req.body.firstname,
@@ -65,13 +65,29 @@ router.post("/signin", (req, res) => {
 /* GET users listing. */
 router.get('/', (req, res) => {
   User.find({})
-  .then((data) => {
-    res.json({ result: true, users: data});
-  })
-  .catch((err) => {
-    res.json({ result: false, error: err.message })
-  })
+    .then((data) => {
+      res.json({ result: true, users: data });
+    })
+    .catch((err) => {
+      res.json({ result: false, error: err.message })
+    })
 });
+
+//getId
+router.get('/id', async (req, res) => {
+  const token = req.headers.authorization
+
+  try {
+    const user = await User.findOne({ token });
+    if (user) {
+      res.json({ result: true, _id: user._id })
+    } else {
+      res.status(404).json({ result: false, message: 'user not found' })
+    }
+  } catch (error) {
+    res.status(500).json({ result: false, message: 'Error server', details: error.message })
+  }
+})
 
 
 // Route pour supprimer un utilisateur
@@ -80,11 +96,11 @@ router.delete('/:id', async (req, res) => {
     const userid = req.params.id
     const deleteUser = await User.findByIdAndDelete(userid)
 
-  res.json({ result: true, message:"Utilisateur supprimé avec succès"});
+    res.json({ result: true, message: "Utilisateur supprimé avec succès" });
   } catch (error) {
-    res.json({result: false, message: 'Utilisateur introuvable'})
-    }
-  });
+    res.json({ result: false, message: 'Utilisateur introuvable' })
+  }
+});
 
 
 module.exports = router;

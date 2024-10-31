8c66567644cfe515b44e4eda12082fa0e49ce3c3
Maxime Sbaizero <maximesbaizero@gmail.com>
2024-10-31 12:41:36 +0100

    ajout des routes pour modifier les orders

--- routes/orders.js
+++ routes/orders.js
@@ -40,44 +40,49 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.get('/expedition', async(req,res)=>{
-    const{order_status} = req.body;
-})
+router.get('/expedition', async (req, res) => {
+    try {
+        const pendingOrders = await Order.find({ order_status: "Pending" });
 
-router.patch('/expedition', async(req,res)=>{
-    const{order_status,expedition_date,tracking_number} = req.body;
+        if (pendingOrders > 0) {
+            res.status(200).json({ result: true, orders: pendingOrders })
+        } else {
+            res.status(404).json({ result: false, message: 'no orders to ship' })
+        }
+    } catch (error) {
+        res.status(500).json({ result: false, message: "server error", details: error.message })
+    }
 })
 
 router.patch('/:id', async (req, res) => {
-    const { expedition_date, tracking_number } = req.body;
- 
+    const { tracking_number, order_status } = req.body;
     try {
-       const updatedOrder = await Order.findByIdAndUpdate(
-        req.params.id, 
-        { expedition_date: Date.now(), tracking_number }, 
-        { new: true });
+        const updatedOrder = await Order.findByIdAndUpdate(
+            req.params.id,
+            {order_status, expedition_date: order_status === "Shipped" ? Date.now() : null, tracking_number },
+            { new: true });
 
-        if(updatedOrder){
-            res.json({result: true, order: updatedOrder})
-        }else{
-            res.status(404).json({result: false, error: 'Order not found'})
+        if (updatedOrder) {
+            res.json({ result: true, order: updatedOrder })
+        } else {
+            res.status(404).json({ result: false, error: 'Order not found' })
         }
     } catch (error) {
-res.status(500).json({result:false, error: 'failed to update order', details: error.message})
+        res.status(500).json({ result: false, error: 'failed to update order', details: error.message })
     }
 })
 
 
-router.get('/:userId' , async (req,res)=>{
+router.get('/:userId', async (req, res) => {
     try {
-        const orders = await Order.find({user:req.params.userId}).populate('articles');
+        const orders = await Order.find({ user: req.params.userId }).populate('articles');
         if (orders.length > 0) {
-            res.status(200).json({result: true, orders})
-        }else{
-            res.status(404).json({result: false, error:' no orders found for user'})
+            res.status(200).json({ result: true, orders })
+        } else {
+            res.status(404).json({ result: false, error: ' no orders found for user' })
         }
     } catch (error) {
-        res.status(500).json({result:false, error:'Failed to get orders', details: error.message})
+        res.status(500).json({ result: false, error: 'Failed to get orders', details: error.message })
     }
 })
 

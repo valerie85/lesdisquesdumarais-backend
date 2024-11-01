acc7f913319fffc75bd8469f253839f14d6802f2
Maxime Sbaizero <maximesbaizero@gmail.com>
2024-11-01 17:30:15 +0100

    mis en commentaire

--- routes/users.js
+++ routes/users.js
@@ -51,10 +51,12 @@ router.post("/signin", (req, res) => {
   }
 
   User.findOne({ email: req.body.email }).then((data) => {
-    if (data.isBan) {
-      res.json({ result: false, error: "Utilisateur banni" });
-      return;
-    }
+
+     if (data.isBan) {
+       res.json({ result: false, error: "Utilisateur banni" });
+       return;
+     }
+
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
       res.json({ result: true, token: data.token, firstName: data.firstname, email: data.email, message: "L'utilisateur est bien connecté" });
     } else {

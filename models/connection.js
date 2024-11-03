d51cf074aa876d7afa9a2f2665628bab3a792889
Your name <Your email>
2024-11-03 15:40:59 +0100

    last changes by maxime

--- models/connection.js
+++ models/connection.js
@@ -4,6 +4,7 @@ const connectionString= process.env.CONNECTION_STRING
 const API_KEY=process.env.API_KEY
 const CONSUMER_KEY=process.env.CONSUMER_KEY
 const CONSUMER_SECRET=process.env.CONSUMER_SECRET
+const BACKEND = process.env.NEXT_PUBLIC_BACKEND
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))

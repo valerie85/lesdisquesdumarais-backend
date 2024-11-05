79656f80d54e1440f85f04c9356041bf5e7ba0ce
Maxime Sbaizero <maximesbaizero@gmail.com>
2024-11-05 14:02:29 +0100

    modif du model ship

--- models/orders.js
+++ models/orders.js
@@ -2,11 +2,19 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
-    order_date: { type: Date, default: Date.now},
+    order_date: { type: Date, default: Date.now },
     total: { type: Number, required: true },
     shipment_operator: { type: String, required: true },
     shipment_price: { type: Number, required: true },
-    shipping_adresse: { type: String, required: true },
+    shipping_adresse: {
+        line1: { type: String },
+        line2: { type: String },
+        line3: { type: String },
+        zip_code: { type: String },
+        city: { type: String },
+        country: { type: String },
+        infos: { type: String }
+    },
     payment_media: { type: String, required: true },
     order_status: { type: String, default: 'Pending' },
     articles: { type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'articles' }], required: true },

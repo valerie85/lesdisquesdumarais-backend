299ceaa987a31b0d01ad7bf18f5a3b7dec12b96b
Maxime Sbaizero <maximesbaizero@gmail.com>
2024-11-17 17:32:33 +0100

    maj app.js suppression des lignes inutiles

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const helmet = require('helmet');
 const xssClean = require('xss-clean');
-const rateLimit = require('express-rate-limit');
 const mongoSanitize = require('express-mongo-sanitize');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,19 +16,17 @@ var shipmentsRouter=require('./routes/shipments');
 //appel middlewares
 var app = express();
 const cors=require('cors');
+
 //controle les origines des requetes
 app.use(cors());
+
 // protege les en-tete http des potentiel attaque 
 app.use(helmet());
+
 // protege contre les attaque de xss (injection de script)
 app.use(xssClean());
-// limitateur de requete par ip configure a 100 requete pour 10 minute
-// app.use(rateLimit({
-//     windowMs: 10 * 60 * 1000,  // 10 minute
-//     max: 100,  //limite de 100
-//     message: 'Trop de requêtes, veuillez réessayer plus tard.'
-//   }));
-  //protege contre les injections MongoDB en supprimant les caractères spéciaux comme $ et .
+
+//protege contre les injections MongoDB en supprimant les caractères spéciaux comme $ et .
 app.use(mongoSanitize());
 app.use(logger('dev'));
 app.use(express.json());
